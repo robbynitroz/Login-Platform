c81ea08b54ec3c5137029b1e7b03f8190e5c2efd
davitdeveloper <davit.aghayan@gmail.com>
2017-10-12 14:49:03 +0400

    creating DB

--- database/migrations/2017_10_12_094535_templatesTableCreation.php
+++ database/migrations/2017_10_12_094535_templatesTableCreation.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class TemplatesTableCreation extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('templates', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('hotel');
+            $table->enum('activated', [0, 1])->default(0);
+            $table->json('data');
+            $table->enum('scheduled', [0, 1])->default(0);
+            $table->timestamp('schedule_start_time')->nullable();
+            $table->timestamp('schedule_end_time')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
@@ -31,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('templates');
     }
 }
--- database/migrations/2017_10_12_113444_hotelsTableCreation.php
+++ database/migrations/2017_10_12_113444_hotelsTableCreation.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class HotelsTableCreation extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('hotels', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 250);
+            $table->string('session_timeout', 50)->default('3d');
             $table->softDeletes();
             $table->timestamps();
         });
@@ -31,6 +29,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
+        Schema::dropIfExists('hotels');
     }
 }

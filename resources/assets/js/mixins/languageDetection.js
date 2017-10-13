fc77cd17f490de247d03e679e44f5f62ef2afc34
davitdeveloper <davit.aghayan@gmail.com>
2017-10-13 15:30:30 +0400

    language detection script & multilang support implemented

--- resources/assets/js/mixins/languageDetection.js
+++ resources/assets/js/mixins/languageDetection.js
@@ -2,25 +2,28 @@ export const languageDetection = {
 
     data() {
         return {
-            defaultLanguage:'en'
+            defaultLanguage: 'en'
         }
     },
 
     mounted() {
         this.$nextTick(function () {
             let userLang = navigator.language || navigator.userLanguage;
-            userLang  = userLang.substring(0, 2);
-            this.checkUserLang(userLang);
+            if (typeof userLang !== "undefined") {
+                userLang = userLang.substring(0, 2);
+
+                this.checkUserLang(userLang);
+            }
         })
     },
 
 
     methods: {
 
-        checkUserLang(userLang){
+        checkUserLang(userLang) {
 
             if (typeof this.texts[userLang] !== "undefined") {
-                this.defaultLanguage=userLang;
+                this.defaultLanguage = userLang;
             }
         }
 

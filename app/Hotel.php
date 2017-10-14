0ae597cb71470ddf3b9e294ef220cdbc7170ad2f
davitdeveloper <davit.aghayan@gmail.com>
2017-10-14 16:26:57 +0400

    building models, migrations, seeders & preparing DB

--- app/Hotel.php
+++ app/Hotel.php
@@ -3,8 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * Class Hotel
+ * @package App
+ */
 class Hotel extends Model
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * @var bool
+     */
+    public $timestamps = true;
+    /**
+     * @var string
+     */
+    protected $table = 'hotels';
+    /**
+     * @var array
+     */
+    protected $fillable = ['name', 'session_timeout'];
+    /**
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
 }

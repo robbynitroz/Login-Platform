3262a3630fd88d37d0e72633ef461607cd165669
davitdeveloper <davit.aghayan@gmail.com>
2017-10-15 18:46:05 +0400

    necessary models & controllers creation

---
+++ app/Http/Controllers/NasController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * Class NasController
+ * @package App\Http\Controllers
+ */
+class NasController extends Controller
+{
+    //
+}
---
+++ app/Nas.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Nas
+ * @package App
+ */
+class Nas extends Model
+{
+    //
+}

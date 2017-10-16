231f07d29736c7ad9848d6376b38c389f96d0ec4
davitdeveloper <davit.aghayan@gmail.com>
2017-10-16 11:19:10 +0400

    necessary models & controllers creation

--- resources/assets/js/store/modules/state.js
+++
@@ -1,9 +0,0 @@
-const state = JSON.parse((document.head.querySelector('script[id="mainData"]')).innerHTML.replace(/^\s+|\s+$/g,''));
-
-    //JSON.parse((document.head.querySelector('script[id="mainData"]')).innerHTML.replace(/^\s+|\s+$/g,''))
-
-
-
-export default {
-    state,
-}
--- resources/assets/js/store/store.js
+++ resources/assets/js/store/store.js
@@ -2,14 +2,12 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import login from './modules/login'
 import home from './modules/home'
-import state from './modules/state'
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
 
     modules: {
-        state,
         login,
         home
     },

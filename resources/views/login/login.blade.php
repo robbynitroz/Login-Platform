e7c0613393f9b46a3345dfc283e3a6f5d9ad10be
davitdeveloper <davit.aghayan@gmail.com>
2017-10-16 15:05:33 +0400

    changing data

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -9,6 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="ip" content="{{ $ip_address }}">
     <script id="mainData" type="application/json">{!! $data->data !!}</script>
+    <link rel="manifest" type="text/plain" href="/manifest.json">
 
     <title>Welcome</title>
 

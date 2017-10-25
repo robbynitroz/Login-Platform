43deff90773bc6da56f5f9b738b645fb48f0edbb
davitdeveloper <davit.aghayan@gmail.com>
2017-10-25 16:09:29 +0400

    client_auth table migration creation

---
+++ database/migrations/2017_10_25_130507_create_client_auths_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientAuthsTable extends Migration
+{
+    /**
+     * Create client_auths table.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_auths', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer()->unsigned()->nullable();
+            $table->string('method', 20);
+            $table->foreign('hotel_id')->references('id')->on('hotels');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Delete client_auths table.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_auths');
+    }
+}

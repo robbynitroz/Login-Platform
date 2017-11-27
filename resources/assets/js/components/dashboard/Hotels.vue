c0527c5755145a7af3dcdeba1c61e9c26331d86a
davitdeveloper <davit.aghayan@gmail.com>
2017-11-27 05:54:18 +0400

    dashboard development

--- resources/assets/js/components/dashboard/Hotels.vue
+++ resources/assets/js/components/dashboard/Hotels.vue
@@ -5,18 +5,59 @@
                 <b-card header="Hotels">
                                     <br/>
                     <b-row>
-                        <b-col lg="6" class="">
+                        <b-col lg="6" class="mb-3">
                             <b-input-group>
                                 <b-input-group-button>
-                                    <b-button>Go!</b-button>
+                                    <b-button variant="success"><i class="fa fa-search"></i></b-button>
                                 </b-input-group-button>
-                                <b-form-input type="text" placeholder="Search for hotel" />
+                                <b-form-input type="text" placeholder="Search for..." />
+                                <b-input-group-button>
+                                    <b-button variant="success">Search</b-button>
+                                </b-input-group-button>
                             </b-input-group>
                         </b-col>
 
+                        <div class="clearfix"></div>
+                        <br/>
 
-                        <b-col lg="12" class="hotels">
-                                <b-table class="table-outline table-responsive-sm mb-0" striped hover :items="items"></b-table>
+                        <b-col lg="12">
+
+                            <b-col lg="4" class="hotels">
+                            <!--Hotel blocks-->
+                            <b-card class="hotels" bg-variant="dark" text-variant="white" title="Hotel CC">
+                                <p class="card-text">
+                                    Hotel description
+                                </p>
+                                <b-button type="edit" size="sm" variant="primary"><i class="fa fa-pencil-square-o"></i> Edit</b-button>
+                                <b-button type="delete" size="sm" variant="danger"><i class="fa fa-ban"></i> Delete</b-button>
+                            </b-card>
+                            </b-col>
+
+                            <b-col lg="4" class="hotels">
+                                <!--Hotel blocks-->
+                                <b-card class="hotels" bg-variant="dark" text-variant="white" title="Hotel CC">
+                                    <p class="card-text">
+                                        Hotel description
+                                    </p>
+                                    <b-button type="edit" size="sm" variant="primary"><i class="fa fa-pencil-square-o"></i> Edit</b-button>
+                                    <b-button type="delete" size="sm" variant="danger"><i class="fa fa-ban"></i> Delete</b-button>
+                                </b-card>
+                            </b-col>
+
+                            <b-col lg="4" class="hotels">
+                                <!--Hotel blocks-->
+                                <b-card class="hotels" bg-variant="dark" text-variant="white" title="Hotel CC">
+                                    <p class="card-text">
+                                        Hotel description
+                                    </p>
+                                    <b-button type="edit" size="sm" variant="primary"><i class="fa fa-pencil-square-o"></i> Edit</b-button>
+                                    <b-button type="delete" size="sm" variant="danger"><i class="fa fa-ban"></i> Delete</b-button>
+                                </b-card>
+                            </b-col>
+
+
+
+
                         </b-col>
 
                     </b-row>
@@ -56,7 +97,9 @@
 </script>
 
 <style>
-    .hotels{
-        border:1px solid #f0f3f5;
-    }
+.hotels{
+    border-radius: 10px;
+    float: left;
+    width: 100%;
+}
 </style>

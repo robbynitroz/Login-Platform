e92235fbb8cdb374743c4c03160d3dd4e09751fd
davitdeveloper <davit.aghayan@gmail.com>
2017-11-29 16:48:02 +0400

    dashboard development

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@dav88dev.com'),
+        'name' => env('MAIL_FROM_NAME', 'Hotel management  platform'),
     ],
 
     /*

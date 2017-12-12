5b764e06cb2c0bc552dc67ad7fdd81d840d9013e
davitdeveloper <davit.aghayan@gmail.com>
2017-12-12 18:55:35 +0400

    dashboard development: template management page development

--- resources/assets/js/components/dashboard/AddTemplate.vue
+++ resources/assets/js/components/dashboard/AddTemplate.vue
@@ -767,7 +767,7 @@
                     this.backgroundColor = 'background:rgba(' +
                         this.colors.rgba.r + ',' + this.colors.rgba.g + ','
                         + this.colors.rgba.b + ',' + this.colors.rgba.a + ')'
-                } else if (section == 'termsColor') {
+                } else if (section == 'policy') {
                     this.policy.color = this.colors.hex
                 } else if (section == 'greeting') {
                     this.greeting.color = this.colors.hex
